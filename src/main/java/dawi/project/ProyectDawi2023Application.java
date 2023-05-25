eca44b4c874f8e3ba6055ad8d5e68353c24d8945
JulioVargas26 <84108359+JulioVargas26@users.noreply.github.com>
2023-05-24 21:00:34 -0500

    master

---
+++ src/main/java/dawi/project/ProyectDawi2023Application.java
@@ -0,0 +1,13 @@
+package dawi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectDawi2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectDawi2023Application.class, args);
+	}
+
+}
